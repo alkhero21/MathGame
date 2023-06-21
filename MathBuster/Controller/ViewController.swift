e3393a882292de583c1f6d60f1f6cefa98f609b6
Alibek <alibek.b1@icloud.com>
2023-06-21 23:18:49 +0600

    first commit

---
+++ MathBuster/Controller/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MathBuster
+//
+//  Created by Alibek Allamzharov on 21.06.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var timerContainerView: UIView!
+    @IBOutlet weak var timerLabel: UILabel!
+    @IBOutlet weak var progressView: UIProgressView!
+    @IBOutlet weak var problemLabel: UILabel!
+    @IBOutlet weak var scoreLabel: UILabel!
+    @IBOutlet weak var resultField: UITextField!
+    @IBOutlet weak var submitButton: UIButton!
+    @IBOutlet weak var restartButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
